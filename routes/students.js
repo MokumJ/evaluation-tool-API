477728727d299144a3529ba6aee14196919c27a5
MokumJ <jesse.vanderheijden@gmail.com>
2017-12-14 11:09:28 +0100

    fix students /get route

--- routes/students.js
+++ routes/students.js
@@ -1,17 +1,20 @@
 // routes/students.js
 const router = require('express').Router()
-const { Student } = require('../models')
+const { Batch, Student } = require('../models')
 const passport = require('../config/auth')
 
-router.get('/students', (req, res, next) => {
-  Student.find()
-    // Newest students first
-    .sort({ createdAt: -1 })
-    // Send the data in JSON format
-    .then((students) => res.json(students))
-    // Throw a 500 error if something goes wrong
-    .catch((error) => next(error))
-  })
+const authenticate = passport.authorize('jwt', { session: false })
+
+  router.get('/students/:id', authenticate, (req, res, next) => {
+        const id = req.params.id
+
+         Student.findById(id)
+          .then((student) => {
+            if (!student) { return next() }
+            res.json(student)
+          })
+          .catch((error) => next(error))
+})
   .get('/students/:id', (req, res, next) => {
     const id = req.params.id
     Student.findById(id)
@@ -21,16 +24,14 @@ router.get('/students', (req, res, next) => {
       })
       .catch((error) => next(error))
   })
-  .post('/students',
-    passport.authorize('jwt', { session: false }),
-    (req, res, next) => {
+  .post('/students', authenticate, (req, res, next) => {
       let newStudent = req.body
-      newStudent.authorId = req.account._id
 
       Student.create(newStudent)
-        .then((student) => res.json(student))
-        .catch((error) => next(error))
-    })
+       .then((student) => res.json(student))
+       .catch((error) => next(error))
+  })
+
   .put('/students/:id', (req, res, next) => {
     const id = req.params.id
     Student.findById(id)
