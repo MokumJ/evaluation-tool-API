ca6aee113a15a729d598814ba77ecaa6d8011ebe
MokumJ <jesse.vanderheijden@gmail.com>
2017-12-14 23:48:11 +0100

    change to number the colors

--- models/student.js
+++ models/student.js
@@ -2,17 +2,16 @@ const mongoose = require('../config/database')
 const { Schema } = mongoose
 
 const evaluationSchema = new Schema({
-  color: { type: String, default: "green" },
+  color: { type: Number, default: 0 },
   date: { type: Date, default: Date.now },
-  context: { type: String, default: false },
+  remark: { type: String, default: false },
 });
 
 const studentSchema = new Schema({
   name: { type: String, required: true },
   picture: { type: String, required: true },
   batchId: { type: Schema.Types.ObjectId, ref: 'batches' }
+  currentColor: { type: Number, default: 0 },
 });
-
-
 
 module.exports = mongoose.model('students', studentSchema)
--- routes/students.js
+++ routes/students.js
@@ -30,23 +30,49 @@ const authenticate = passport.authorize('jwt', { session: false })
       Student.create(newStudent)
        .then((student) => res.json(student))
        .catch((error) => next(error))
-  })
+     })
+  .put('/students/:id', authenticate, (req, res, next) => {
+        const id = req.params.id
 
-  .put('/students/:id', (req, res, next) => {
-    const id = req.params.id
-    Student.findById(id)
-      .then((student) => {
-        if (!student) { return next() }
 
-        const newData = req.body
 
-        student.update(newData)
-          .then((updatedStudent) => {
-            res.json(updatedStudent)
-          })
-          .catch((error) => next(error))
-      })
-      .catch((error) => next(error))
-  })
 
-module.exports = router
+         Student.findByIdAndUpdate(id, { $set: evaluateStudent }, { new: true })
+           .then((student) => res.json(student))
+           .catch((error) => next(error))
+        })
+
+      .patch('/students/:id', authenticate, (req, res, next) => {
+        const id = req.params.id
+        const evaluateStudent = req.body
+
+        Student.findById(id)
+         .then((student) => {
+           if (!student) { return next() }
+           student.currentColor = (student.studentEval.sort({ evalDay: 1})[student.studentEval.length - 1].evalColor)
+
+
+           Student.findByIdAndUpdate(id, { $set: student }, { new: true })
+             .then((student) => res.json(student))
+             .catch((error) => next(error))
+         })
+         .catch((error) => next(error))
+    })
+
+    .delete('/students/:id', authenticate, (req, res, next) => {
+      const id = req.params.id
+
+        Student.findByIdAndRemove(id)
+           .then(() => {
+                 res.status = 200
+                 res.json({
+                   message: 'Removed',
+                   _id: id
+                 })
+               })
+           .catch((error) => next(error))
+       })
+
+
+
+    module.exports = router
